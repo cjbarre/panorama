0bfc334080fc5f1a7fb0297f732f78f57f6f3be6
cjbarre <cjbarre@gmail.com>
2023-07-04 13:53:20 -0500

    Get rid of biff user schemas.

--- backend/src/com/be/schema.clj
+++ backend/src/com/be/schema.clj
@@ -6,23 +6,7 @@
 (defn anomaly? [x] (when (::anom/category x) x))
 
 (def schema
-  {:user/id :uuid
-   :user/email :string
-   :user/joined-at inst?
-   :user [:map {:closed true}
-          [:xt/id :user/id]
-          :user/email
-          :user/joined-at]
-
-   :msg/id :uuid
-   :msg/user :user/id
-   :msg/text :string
-   :msg/sent-at inst?
-   :msg [:map {:closed true}
-         [:xt/id :msg/id]
-         :msg/user
-         :msg/text
-         :msg/sent-at]})
+  {})
 
 (def features
   {:schema schema})
